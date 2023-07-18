fb34d9354d1a84d42a2b9d701b996d5089179da3
suyash-thakur <500067151@stu.upes.ac.in>
2023-07-19 01:15:16 +0530

    fix for evaluator test

--- test/evaluator.test.js
+++ test/evaluator.test.js
@@ -28,18 +28,16 @@ tap.test('Evaluator', (t) => {
         ],
         groupBy: {
             columns: [{ type: 'column', name: 'column1' }],
-            having: [{ type: 'column', name: 'column2', operator: '>', value: '5' }],
+            having: [{ type: 'column', name: 'column1', operator: '>', value: '5' }],
         },
         orderBy: [{ type: 'column', name: 'column1', order: 'ASC' }],
-        limit: { value: '10' },
+        limit: { value: '2' },
     };
 
     t.test('evaluateWhereExpression - Evaluates WHERE condition', (t) => {
         const evaluator = new Evaluator(ast);
-
         const result = evaluator.evaluateWhereExpression(rows[0]);
         t.equal(result, true, 'WHERE condition evaluates correctly');
-
         t.end();
     });
 
@@ -49,7 +47,7 @@ tap.test('Evaluator', (t) => {
         const result = evaluator.evaluateGroupByAst(rows);
         const expectedResult = {
             '10': [{ column1: 10, column2: 20 }],
-            '15': [{ column1: 15, column2: 25 }],
+            '15': [{ column1: 15, column2: 25 }]
         };
 
         t.deepEqual(result, expectedResult, 'GROUP BY clause evaluates correctly');
@@ -76,12 +74,12 @@ tap.test('Evaluator', (t) => {
             '15': [{ column1: 15, column2: 25 }],
         };
 
-        const groupByColumns = ['column1'];
-
+        const groupByColumns = evaluator.getGroupByColumns();
         const result = evaluator.evaluateSelectForGroupBy(data, groupByColumns);
+
         const expectedResult = [
-            { column1: 10, column2: 20 },
-            { column1: 15, column2: 25 },
+            { column1: [10], column2: [20] },
+            { column1: [15], column2: [25] },
         ];
 
         t.deepEqual(result, expectedResult, 'SELECT clause with GROUP BY evaluates correctly');
@@ -91,12 +89,34 @@ tap.test('Evaluator', (t) => {
 
     t.test('evaluateOrderByAst - Evaluates ORDER BY clause', (t) => {
         const evaluator = new Evaluator(ast);
-
-        const result = evaluator.evaluateOrderByAst(rows);
+        const testData = [
+            {
+                column1: [10],
+                column2: [20],
+            },
+            {
+                column1: [15],
+                column2: [25],
+            },
+            {
+                column1: [5],
+                column2: [15],
+            },
+        ];
+        const result = evaluator.evaluateOrderByAst(testData);
         const expectedResult = [
-            { column1: 10, column2: 20 },
-            { column1: 15, column2: 25 },
-            { column1: 5, column2: 15 },
+            {
+                column1: [5],
+                column2: [15],
+            },
+            {
+                column1: [10],
+                column2: [20],
+            },
+            {
+                column1: [15],
+                column2: [25],
+            },
         ];
 
         t.deepEqual(result, expectedResult, 'ORDER BY clause evaluates correctly');
@@ -107,12 +127,26 @@ tap.test('Evaluator', (t) => {
     t.test('evaluateSelectAst - Evaluates SELECT clause', (t) => {
         const evaluator = new Evaluator(ast);
 
-        const result = evaluator.evaluateSelectAst(rows);
-        const expectedResult = {
-            column1: [10, 5, 15],
-            column2: [20, 15, 25],
-        };
+        const testData = [
+            {
+                column1: [10],
+                column2: [20],
+            },
+            {
+                column1: [15],
+                column2: [25],
+            },
+            {
+                column1: [5],
+                column2: [15],
+            },
+        ];
 
+        const result = evaluator.evaluateSelectAst(testData);
+        const expectedResult = {
+            column1: [10, 15, 5],
+            column2: [20, 25, 15],
+        };
         t.deepEqual(result, expectedResult, 'SELECT clause evaluates correctly');
 
         t.end();
@@ -121,10 +155,31 @@ tap.test('Evaluator', (t) => {
     t.test('evaluateLimitAst - Evaluates LIMIT clause', (t) => {
         const evaluator = new Evaluator(ast);
 
-        const result = evaluator.evaluateLimitAst(rows);
+        const testData = [
+            {
+                column1: [10],
+                column2: [20],
+            },
+            {
+                column1: [15],
+                column2: [25],
+            },
+            {
+                column1: [5],
+                column2: [15],
+            },
+        ];
+
+        const result = evaluator.evaluateLimitAst(testData);
         const expectedResult = [
-            { column1: 10, column2: 20 },
-            { column1: 5, column2: 15 },
+            {
+                column1: [10],
+                column2: [20],
+            },
+            {
+                column1: [15],
+                column2: [25],
+            },
         ];
 
         t.deepEqual(result, expectedResult, 'LIMIT clause evaluates correctly');
